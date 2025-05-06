1d1a2a4f7c84ec336933356dfd4cc040039d139f
Noel Del Castillo <noel@pragtechnologies.com>
2025-05-06 13:23:47 +0800

    Initial commit

---
+++ lib/ex_litellm.ex
@@ -0,0 +1,18 @@
+defmodule ExLitellm do
+  @moduledoc """
+  Documentation for `ExLitellm`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLitellm.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_litellm_test.exs
@@ -0,0 +1,8 @@
+defmodule ExLitellmTest do
+  use ExUnit.Case
+  doctest ExLitellm
+
+  test "greets the world" do
+    assert ExLitellm.hello() == :world
+  end
+end
