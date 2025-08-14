e1aa9f2e630558bb152f7628306ff5735caf15b1
djhayar72 <dagooc.jayar@gmail.com>
2025-08-14 15:43:06 +0800

    Remove wav sample generator

--- lib/ex_litellm/model.ex
+++ lib/ex_litellm/model.ex
@@ -21,18 +21,4 @@ defmodule ExLitellm.Model do
   end
 
   def audio_transcribe(_), do: {:error, "You must pass a valid Multipart struct"}
-
-  # 1-second 8-bit PCM silent WAV file
-  # 1-second silent WAV (8-bit PCM)
-  @base64_wav "UklGRiQAAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAZGF0YQAAAAA="
-
-  def write_sample_wav(path \\ "test/fixtures/sample.wav") do
-    dir = Path.dirname(path)
-    File.mkdir_p!(dir)
-
-    wav_binary = Base.decode64!(@base64_wav)
-    File.write!(path, wav_binary)
-
-    path
-  end
 end
